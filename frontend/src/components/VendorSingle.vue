0e190dfd324dc422447eb346d063f0692ab611d9
Tyler Olson <tolson519@gmail.com>
2025-04-21 12:56:48 -0700

    sort table when clicking on header

--- frontend/src/components/VendorSingle.vue
+++ frontend/src/components/VendorSingle.vue
@@ -1,14 +1,49 @@
 <script setup lang="ts">
 import { FetchVendor, GetVendor } from "../../wailsjs/go/main/App"
 
-import { onMounted, ref } from "vue"
+import { onMounted, ref, computed } from "vue"
 import { wfmplatefficiency } from "../../wailsjs/go/models"
+// import { LogPrint } from "../../wailsjs/runtime/"
 
 const props = defineProps<{
   name: string
 }>()
 
+const fetchButton = ref("Fetch")
+
+const sortKey = ref("name")
+const sortAsc = ref(true)
+
 const items = ref<wfmplatefficiency.Item[]>([])
+
+const sortedItems = computed(() => {
+  if (!sortKey.value) {
+    return [...items.value]
+  }
+
+  const sortedArray = [...items.value].sort((a, b) => {
+    const valueA = a[sortKey.value as keyof wfmplatefficiency.Item]
+    const valueB = b[sortKey.value as keyof wfmplatefficiency.Item]
+
+    if (valueA < valueB) {
+      return sortAsc.value ? -1 : 1
+    }
+    if (valueA > valueB) {
+      return sortAsc.value ? 1 : -1
+    }
+    return 0
+  })
+  return sortedArray
+})
+
+function sort(key: keyof wfmplatefficiency.Item) {
+  if (sortKey.value === key) {
+    sortAsc.value = !sortAsc.value
+  } else {
+    sortKey.value = key
+    sortAsc.value = true
+  }
+}
 
 function getItems() {
   GetVendor(props.name).then((result: wfmplatefficiency.Vendor) => {
@@ -17,8 +52,10 @@ function getItems() {
 }
 
 async function fetch() {
-  await FetchVendor(props.name).then(result => {})
+  fetchButton.value = "Fetching..."
+  await FetchVendor(props.name)
   getItems()
+  fetchButton.value = "Fetched"
 }
 
 onMounted(() => {
@@ -29,21 +66,27 @@ onMounted(() => {
 <template>
   <div class="flex justify-center">
     <div class="w-full overflow-scroll">
-      <p>{{ name }}</p>
-      <button @click="fetch()" class="cursor-pointer rounded-md bg-gray-400 px-4 py-1">Fetch</button>
-
+      <div class="flex items-center justify-center py-5">
+        <span class="text-4xl">{{ name }}</span>
+        <button
+          @click="fetch()"
+          class="ml-5 cursor-pointer rounded-md bg-gray-400 px-4 py-1 transition hover:bg-gray-500"
+        >
+          {{ fetchButton }}
+        </button>
+      </div>
       <table class="w-full">
         <thead class="bg-gray-700 text-xs text-gray-400 uppercase">
           <tr>
-            <th class="py-3">Name</th>
-            <th class="py-3">Type</th>
-            <th class="py-3">Standing</th>
-            <th class="py-3">Average Volume</th>
-            <th class="py-3">Average Price</th>
+            <th @click="sort('name')" class="cursor-pointer py-3 select-none">Name</th>
+            <th @click="sort('type')" class="cursor-pointer py-3 select-none">Type</th>
+            <th @click="sort('standing')" class="cursor-pointer py-3 select-none">Standing</th>
+            <th @click="sort('AvgVol')" class="cursor-pointer py-3 select-none">Average Volume</th>
+            <th @click="sort('WeightedAvgPrice')" class="cursor-pointer py-3 select-none">Average Price</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="item in items" :key="item.name">
+          <tr v-for="item in sortedItems" :key="item.name">
             <td>{{ item.name }}</td>
             <td>{{ item.type }}</td>
             <td>{{ item.standing }}</td>
